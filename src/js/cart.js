d0b073b5bf68a3e5a75e1f4c38b8b58899ca2d31
Fernando Gómez <davidf.gomez@hotmail.com>
2023-10-26 12:57:23 -0500

    cartcount styles

--- src/js/cart.js
+++ src/js/cart.js
@@ -7,6 +7,7 @@ export const RenderCartCount = (parent, btn, amountElm) => {
     if (parseInt(amountElm.textContent) > 0) {
       carCount++
       const elm = document.createElement('SPAN')
+      elm.classList.add('cart__count')
       elm.textContent = carCount
       parent.prepend(elm)
     }
