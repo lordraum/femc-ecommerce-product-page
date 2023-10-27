1fa77a7f2a3bf27553d306663a62033ebff19bad
Fernando Gómez <davidf.gomez@hotmail.com>
2023-10-27 08:48:28 -0500

    Correción changeProdImage

--- src/js/change_product_image.js
+++ src/js/change_product_image.js
@@ -1,22 +1,29 @@
+import productImage01 from '../images/image-product-1.jpg'
+import productImage02 from '../images/image-product-2.jpg'
+import productImage03 from '../images/image-product-3.jpg'
+import productImage04 from '../images/image-product-4.jpg'
+
+const productImages = [productImage01, productImage02, productImage03, productImage04]
+
 export const changeProductImageMobile = (controls, image, num) => {
   let currentImage = parseInt(image.dataset.num)
   controls.addEventListener('click', (e) => {
     const arrowType = e.target.dataset.type
     if (arrowType === 'next') {
       if (currentImage === num) {
-        image.attributes.src.value = './src/images/image-product-1.jpg'
+        image.src = productImage01
         currentImage = 1
       } else {
-        image.attributes.src.value = `./src/images/image-product-${currentImage + 1}.jpg`
+        image.src = productImages[currentImage]
         currentImage++
       }
     }
     if (arrowType === 'previous') {
       if (currentImage === 1) {
-        image.attributes.src.value = './src/images/image-product-4.jpg'
+        image.src = productImage04
         currentImage = 4
       } else {
-        image.attributes.src.value = `./src/images/image-product-${currentImage - 1}.jpg`
+        image.src = productImages[currentImage - 2]
         currentImage--
       }
     }
