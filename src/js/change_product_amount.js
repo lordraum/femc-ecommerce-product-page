bd320875958b01c8f0975239eb0159691bdac86b
Fernando Gómez <davidf.gomez@hotmail.com>
2023-10-31 12:07:06 -0500

    Corregido amountControls JS

--- src/js/change_product_amount.js
+++ src/js/change_product_amount.js
@@ -1,18 +1,15 @@
 const changeProductAmount = (controls, amountElm, stop) => {
-  let amount = parseInt(amountElm.textContent)
   controls.addEventListener('click', (e) => {
     const controlType = e.target.dataset.type
 
     if (controlType === 'plus') {
-      if (amount >= 0 && amount < stop) {
-        amount++
-        amountElm.textContent = amount
+      if (amountElm.textContent >= 0 && amountElm.textContent < stop) {
+        amountElm.textContent++
       }
     }
     if (controlType === 'minus') {
-      if (amount >= 1) {
-        amount--
-        amountElm.textContent = amount
+      if (amountElm.textContent >= 1) {
+        amountElm.textContent--
       }
     }
   })
